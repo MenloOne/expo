8b788e30c305e03573e21796905828328e3ea8e0
daviddfm <david@dawson.fm>
2018-07-03 19:40:06 +0200

    fix

--- src/storage/JavascriptIPFSStorage.js
+++ src/storage/JavascriptIPFSStorage.js
@@ -1,5 +1,5 @@
 /*
- * 
+ *
  *
  * Licensed under the Apache License, Version 2.0 (the “License”);
  * you may not use this file except in compliance with the License.
@@ -18,36 +18,36 @@ import IPFS from 'ipfs';
 import HashUtils from 'HashUtils';
 
 class JavascriptIPFSStorage {
-  constructor() {
-    this.ipfs = new IPFS();
-    this.messagesList = [];
-    this.connectedToPeer = false;
-  }
+    constructor() {
+        this.ipfs = new IPFS();
+        this.messagesList = [];
+        this.connectedToPeer = false;
+    }
 
-  createMessage(message) {
-    return new Promise((resolve, reject) => {
-      HashUtils.nodeToCID(message, (cidErr, cid) => {
-        this.ipfs.dag.put(message, {cid: cid}, (putErr, result) => {
-          this.messagesList.push(message);
-          resolve(result.toBaseEncodedString());
+    createMessage(message) {
+        return new Promise((resolve, reject) => {
+            HashUtils.nodeToCID(message, (cidErr, cid) => {
+                this.ipfs.dag.put(message, {cid: cid}, (putErr, result) => {
+                    this.messagesList.push(message);
+                    resolve(result.toBaseEncodedString());
+                })
+            })
         })
-      })
-    })
-  }
+    }
 
-  findMessage(hash) {
-    return new Promise((resolve, reject) => {
-      this.ipfs.dag.get(hash, (err, result) => {
-        result ? resolve({...result.value, hash: hash}) : resolve(null)
-      })
-    })
-  }
+    findMessage(hash) {
+        return new Promise((resolve, reject) => {
+            this.ipfs.dag.get(hash, (err, result) => {
+                result ? resolve({...result.value, hash: hash}) : resolve(null)
+            })
+        })
+    }
 
-  connectPeer(remote) {
-    this.ipfs.on('ready', () => {
-      try {
-          new Promise((resolve, reject) => {
-            try {
+    connectPeer(remote) {
+        return;
+
+        this.ipfs.on('ready', () => {
+            new Promise((resolve, reject) => {
                 remote.connection.id((err, result) => {
                     if (err) {
                         reject(err);
@@ -64,15 +64,13 @@ class JavascriptIPFSStorage {
                         }
                     })
                 })
-            } catch (e => {})
-          })
-      } catch(e => {})
-    })
-  }
+            })
+        })
+    }
 
-  isOnline() {
-    return this.ipfs.isOnline() && this.connectedToPeer;
-  }
+    isOnline() {
+        return this.ipfs.isOnline() && this.connectedToPeer;
+    }
 }
 
 export default JavascriptIPFSStorage;
