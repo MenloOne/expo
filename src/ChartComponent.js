eec30d2cdbb3638a2164d067690e0a9caa793865
MontaGhanmy <monta.ghanmy@gmail.com>
2018-07-03 19:41:04 +0100

    more updates to the API

--- src/ChartComponent.js
+++ src/ChartComponent.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import {Line} from 'react-chartjs-2';
 import axios from 'axios';
-
+import config from './internals/config/private';
 
 var dataOne = {
     labels: [
