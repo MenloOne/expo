925d5a036cf274cde415cf890e07d987a1c8a1cf
daviddfm <david@dawson.fm>
2018-08-12 20:34:27 +0200

    try infura https:5001

--- src/EthContext.js
+++ src/EthContext.js
@@ -7,9 +7,9 @@ import EthereumLottery from './contracts/EthereumLottery'
 import MessageBoardGraph from './storage/MessageBoardGraph'
 import Client, {FakeClient} from './Client'
 
-// const remoteStorage = new RemoteIPFSStorage({host: 'ipfs.infura.io', port: '5001', protocol: 'https'})
 // const remoteStorage = new RemoteIPFSStorage('/ip4/127.0.0.1/tcp/5001')
-const remoteStorage = new RemoteIPFSStorage({host: 'gateway.ipfs.io', port: '443', protocol: 'https', 'api-path': 'ipfs/api/v0/' })
+// const remoteStorage = new RemoteIPFSStorage({host: '18.216.45.165', port: '5001', protocol: 'https'})
+const remoteStorage = new RemoteIPFSStorage({host: 'ipfs.infura.io', port: '5001', protocol: 'https'})
 const localStorage = new JavascriptIPFSStorage()
 localStorage.connectPeer(remoteStorage)
 
