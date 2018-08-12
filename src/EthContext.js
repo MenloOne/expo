e1b85503a8856bc01a2d00528710c03a3015fb8e
daviddfm <david@dawson.fm>
2018-08-12 17:15:49 +0200

    try IPFS Menlo on AWS

--- src/EthContext.js
+++ src/EthContext.js
@@ -9,7 +9,7 @@ import Client, {FakeClient} from './Client'
 
 // const remoteStorage = new RemoteIPFSStorage({host: 'ipfs.infura.io', port: '5001', protocol: 'https'})
 // const remoteStorage = new RemoteIPFSStorage('/ip4/127.0.0.1/tcp/5001')
-const remoteStorage = new RemoteIPFSStorage({host: 'ipfs.menlo.one', port: '443', protocol: 'https'})
+const remoteStorage = new RemoteIPFSStorage('/ip4/18.216.45.165/tcp/5001')
 const localStorage = new JavascriptIPFSStorage()
 localStorage.connectPeer(remoteStorage)
 
