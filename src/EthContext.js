c9f09cb0a13a5fc2a175515c08b3f8a9b1748544
daviddfm <david@dawson.fm>
2018-08-12 21:22:51 +0200

    switch use of ipfs constructor

--- src/EthContext.js
+++ src/EthContext.js
@@ -9,7 +9,7 @@ import Client, {FakeClient} from './Client'
 
 // const remoteStorage = new RemoteIPFSStorage('/ip4/127.0.0.1/tcp/5001')
 // const remoteStorage = new RemoteIPFSStorage({host: '18.216.45.165', port: '5001', protocol: 'https'})
-const remoteStorage = new RemoteIPFSStorage({host: 'ipfs.infura.io', port: '5001', protocol: 'https'})
+const remoteStorage = new RemoteIPFSStorage('ipfs.infura.io', '5001', { protocol: 'https' })
 const localStorage = new JavascriptIPFSStorage()
 localStorage.connectPeer(remoteStorage)
 
