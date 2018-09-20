23b0174edd0e739a84b4f275fec070d57375e4a5
Rick Bross <rbross3@gmail.com>
2018-09-20 11:09:37 -0400

    perfect styles on <header><nav>

--- src/TopNav.js
+++ src/TopNav.js
@@ -10,7 +10,7 @@ import MenloFaucetContract from './truffle_artifacts/contracts/MenloFaucet.json'
 
 import './css/sb-admin.css'
 
-const logo = require('./images/logo.jpg')
+const logo = require('./images/logo.svg')
 
 class TopNav extends Component {
 
